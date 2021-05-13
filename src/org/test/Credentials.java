b78fa0ebc40d2c417a5e3b5a7943e53bb54b16a9
Salman-Testing <salman81194@gmail.com>
2021-05-13 09:02:10 +0530

    New Updates

--- src/org/test/Credentials.java
+++ src/org/test/Credentials.java
@@ -2,4 +2,11 @@ package org.test;
 
 public class Credentials {
 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("Welcome");
+		System.out.println("to Selenium");
+	}
 }
